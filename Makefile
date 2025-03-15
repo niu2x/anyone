37af7b1bf6af9b77314d986651d8c9c55401e9e4
niu2x <niu2x@icloud.com>
2025-03-16 01:02:08 +0800

    fix

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	cmake -S. -Bbuild 
+	cmake -S. -Bbuild -DBoost_DIR=/home/niu2x/project/boost/dist/linux/lib/cmake/Boost-1.84.0
 	cmake --build build -j 2
 
 .PHONY: build
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <nx/alias.h>
 
+#include <boost/program_options.hpp>
+#include <boost/variant/variant.hpp>
+#include <boost/variant/get.hpp>
+
+namespace po = boost::program_options;
+
 int main() {
 	return 0;
 }
