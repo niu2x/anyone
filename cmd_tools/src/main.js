d2421d8b4c7815424a1de28bfc1086310571304f
niu2x <niu2x@icloud.com>
2025-03-16 14:02:21 +0800

    fix

--- cmd_tools/src/main.js
+++ cmd_tools/src/main.js
@@ -1,3 +1,35 @@
 #!/usr/bin/env node
 
-console.log("anyone")
+import {
+  Command
+} from 'commander';
+
+import path from "path"
+import fs from "fs";
+
+
+const program = new Command();
+
+program
+  .name('anyone')
+
+
+program.command("init")
+  .argument('<dir>', 'project directory')
+  .action((dir) => {
+    const configPath = path.join(dir, "project.json")
+    const projectConfig = {
+      "name": "Anyone Game",
+      "entry": "src/entry.lua"
+    }
+    fs.writeFileSync(configPath, JSON.stringify(projectConfig, null, "  "))
+  })
+
+program.command("run")
+  .argument('<dir>', 'project directory')
+  .action((dir) => {
+    console.log(`init ${dir}`)
+  })
+
+// registerSubCommand(program)
+program.parse(process.argv);
