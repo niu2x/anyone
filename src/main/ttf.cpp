f2e69138faa1798f256b853c485d4a41e3d016cf
niu2x <niu2x@icloud.com>
2025-03-17 20:08:13 +0800

    fix

--- src/main/ttf.cpp
+++ src/main/ttf.cpp
@@ -11,3 +11,23 @@ FreeTypeLibrary::FreeTypeLibrary()
 FreeTypeLibrary::~FreeTypeLibrary() { FT_Done_FreeType(ft_library_); }
 
 } // namespace anyone
+
+namespace anyone {
+
+MiniFont::MiniFont(int width, int height, int cell_size) : cell_size_(cell_size)
+{
+    texture_ = new GL_Texture2D(width, height);
+}
+
+MiniFont::~MiniFont() { delete texture_; }
+
+void MiniFont::build_ascii_chars(const uint8_t* ttf_font_data)
+{
+    build_chars(ttf_font_data,
+                "01234567890abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXY"
+                "Z:;'\"[]{}!@#$%^&*()-=_+,.<>/?|\\~` ");
+}
+
+void MiniFont::build_chars(const uint8_t* ttf_font_data, const char* chars) { }
+
+} // namespace anyone
--- src/main/ttf.h
+++ src/main/ttf.h
@@ -17,4 +17,16 @@ private:
     FT_Library ft_library_;
 };
 
+class MiniFont {
+public:
+    MiniFont(int width, int height, int cell_size);
+    ~MiniFont();
+    void build_ascii_chars(const uint8_t* ttf_font_data);
+    void build_chars(const uint8_t* ttf_font_data, const char* chars);
+
+private:
+    GL_Texture2D* texture_;
+    int cell_size_;
+};
+
 } // namespace anyone
