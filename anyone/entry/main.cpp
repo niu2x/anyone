3835d0245927662e84f5a1b843b8e5167a7da945
niu2x <niu2x@icloud.com>
2025-03-27 11:16:55 +0800

    fix

--- anyone/entry/main.cpp
+++ anyone/entry/main.cpp
@@ -1,20 +1,39 @@
 #include "platform/linux/support.h"
 #include "main/core.h"
+#include "3rd/cxxopts.hpp"
 
 using namespace anyone;
 
-static void run(PlatformSupport* platform_support)
+static void run(StartupConfig& config)
 {
     Core core;
+    core.set_startup_config(config);
+
+    auto platform_support = GET_PLATFORM_SUPPORT();
     platform_support->init_window();
     while (platform_support->poll_events()) {
         platform_support->swap_buffers();
     }
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+    cxxopts::Options options("anyone_rt", "anyone runtime");
+    // clang-format off
+    options.add_options()
+        ("project", "project dir", cxxopts::value<std::string>())
+        ;
+    // clang-format on
+
+    auto result = options.parse(argc, argv);
+    auto project_dir = result["project"].as<std::string>();
     anyone::PlatformLinux platform_linux;
-    run(&platform_linux);
+
+    StartupConfig config = {
+        .platform_support = &platform_linux,
+        .project_dir = project_dir,
+    };
+
+    run(config);
     return 0;
 }
