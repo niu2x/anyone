e090ca30b62049a16058976f7d39bd6c9ce91960
niu2x <niu2x@icloud.com>
2025-03-30 23:31:14 +0800

    fix

--- anyone/main/rml_ui.cpp
+++ anyone/main/rml_ui.cpp
@@ -1,12 +1,13 @@
 #include "rml_ui.h"
 #include "base/type.h"
 #include <RmlUi/Debugger.h>
+#include <RmlUi/Core.h>
 #include "embed/default_ttf.h"
 
 const char* demo = R"RAW(
 <rml>
 <head>
-     <link type="text/rcss" href="test.rcss"/>
+     <link type="text/rcss" href="builtin:///test.rcss"/>
 </head>
 <body>
     Pasta
@@ -17,19 +18,165 @@ const char* demo = R"RAW(
 
 namespace anyone {
 
-rml_ui::MyRenderInterface* RML_UI::render_impl_ = nullptr;
-rml_ui::MySystemInterface* RML_UI::system_impl_ = nullptr;
-Material* RML_UI::rml_ui_material_ = nullptr;
+using namespace Rml;
+
+class MySystemInterface : public Rml::SystemInterface {
+public:
+    MySystemInterface() { }
+    ~MySystemInterface() { }
+};
+
+struct GPU_Vertex {
+    float x, y;
+    float u, v;
+    uint8_t r, g, b, a;
+};
+
+class MyRenderInterface : public Rml::RenderInterface {
+public:
+    MyRenderInterface() : material_(nullptr) { }
+    ~MyRenderInterface() { }
+    CompiledGeometryHandle CompileGeometry(Span<const Vertex> vertices,
+                                           Span<const int> indices) override
+    {
+
+        auto vbo = GET_RENDER_API()->create_vertex_buffer();
+        vbo->alloc_cpu_buffer(sizeof(GPU_Vertex) * vertices.size());
+        auto buf = (GPU_Vertex*)vbo->get_cpu_buffer();
+        for (int i = 0; i < vertices.size(); i++) {
+            buf[i].x = vertices[i].position[0];
+            buf[i].y = vertices[i].position[1];
+            buf[i].r = vertices[i].colour[0];
+            buf[i].g = vertices[i].colour[1];
+            buf[i].b = vertices[i].colour[2];
+            buf[i].a = vertices[i].colour[3];
+            buf[i].u = vertices[i].tex_coord[0];
+            buf[i].v = vertices[i].tex_coord[1];
+        }
+
+        vbo->set_vertex_layout({
+            VertexAttr::POSITION_XY,
+            VertexAttr::UV,
+            VertexAttr::COLOR_BYTE_RGBA,
+        });
+
+        vbo->apply();
+        vbo->free_cpu_buffer();
+
+        IndiceBuffer* veo = GET_RENDER_API()->create_indice_buffer();
+        veo->alloc_cpu_buffer(indices.size());
+        auto indice_buf = veo->get_cpu_buffer();
+        for (int i = 0; i < indices.size(); i++) {
+            indice_buf[i] = indices[i];
+        }
+        veo->apply();
+        veo->free_cpu_buffer();
+
+        auto container = new VertexIndiceBuffer;
+        container->vbo = vbo;
+        container->veo = veo;
+
+        return (uintptr_t)container;
+    }
+
+    void RenderGeometry(CompiledGeometryHandle geometry,
+                        Vector2f translation,
+                        TextureHandle texture) override
+    {
+
+        float offset[2] = { translation[0], translation[1] };
+
+        MaterialParam params[3] = {
+            { .name = "tex",
+              .value = { MaterialParamType::TEXTURE, { .tex_unit = 0 } } },
+            { .name = "offset",
+              .value = { MaterialParamType::VEC2, { .args = offset } } },
+            { .name = "canvas_size",
+              .value = { MaterialParamType::VEC2, { .args = canvas_size_ } } },
+        };
+
+        auto container = (VertexIndiceBuffer*)geometry;
+        GET_RENDER_API()->draw(DrawOperation {
+            .polygon_mode = PolygonMode::FILL,
+            .vertex_buffer = container->vbo,
+            .indice_buffer = container->veo,
+            .count = container->veo->get_indice_count(),
+            .texture = (Texture2D*)texture,
+            .textures = nullptr,
+            .material = material_,
+            .material_params = params,
+            .material_params_count = 3,
+        });
+    }
+
+    void ReleaseGeometry(CompiledGeometryHandle geometry) override
+    {
+        auto container = (VertexIndiceBuffer*)geometry;
+        GET_RENDER_API()->destroy_vertex_buffer(container->vbo);
+        GET_RENDER_API()->destroy_indice_buffer(container->veo);
+        delete container;
+    }
+
+    TextureHandle LoadTexture(Vector2i& texture_dimensions,
+                              const String& source) override
+    {
+        return 0;
+    }
+
+    TextureHandle GenerateTexture(Span<const byte> source,
+                                  Vector2i source_dimensions) override
+    {
+
+        auto tex = GET_RENDER_API()->create_texture_2d();
+        tex->alloc_cpu_buffer(source_dimensions[0], source_dimensions[1]);
+        memcpy(tex->get_cpu_buffer(), source.data(), source.size());
+        tex->apply();
+        tex->free_cpu_buffer();
+        return (uintptr_t)tex;
+    }
+
+    void ReleaseTexture(TextureHandle texture) override
+    {
+        GET_RENDER_API()->destroy_texture_2d((Texture2D*)texture);
+    }
+
+    void EnableScissorRegion(bool enable) override { }
+    void SetScissorRegion(Rectanglei region) override { }
+
+    void set_material(Material* m) { material_ = m; }
+    void set_canvas_size(const IntSize& s)
+    {
+        canvas_size_[0] = s.width;
+        canvas_size_[1] = s.height;
+    }
+
+private:
+    Material* material_;
+    float canvas_size_[2];
+};
+
+class MyFileInterface : public FileInterface {
+public:
+    MyFileInterface() { }
+    ~MyFileInterface() { }
+};
+
+MyRenderInterface* render_impl_ = nullptr;
+MySystemInterface* system_impl_ = nullptr;
+MyFileInterface* file_impl_ = nullptr;
+Material* rml_ui_material_ = nullptr;
 
 void RML_UI::setup()
 {
-    render_impl_ = new rml_ui::MyRenderInterface;
-    system_impl_ = new rml_ui::MySystemInterface;
+    render_impl_ = new MyRenderInterface;
+    system_impl_ = new MySystemInterface;
+    file_impl_ = new MyFileInterface;
     rml_ui_material_ = GET_RENDER_API()->create_rml_ui_material();
     render_impl_->set_material(rml_ui_material_);
 
     Rml::SetRenderInterface(render_impl_);
     Rml::SetSystemInterface(system_impl_);
+    Rml::SetFileInterface(file_impl_);
     Rml::Initialise();
 
     Rml::LoadFontFace({ default_ttf, default_ttf_length },
@@ -43,6 +190,7 @@ void RML_UI::cleanup()
     GET_RENDER_API()->destroy_material(rml_ui_material_);
     delete system_impl_;
     delete render_impl_;
+    delete file_impl_;
 }
 
 RML_UI::RML_UI()
@@ -94,137 +242,3 @@ void RML_UI::render()
 }
 
 } // namespace anyone
-
-namespace anyone::rml_ui {
-
-MyRenderInterface::MyRenderInterface() : material_(nullptr) { }
-MyRenderInterface::~MyRenderInterface() { }
-
-MySystemInterface::MySystemInterface() { }
-MySystemInterface::~MySystemInterface() { }
-
-CompiledGeometryHandle
-MyRenderInterface::CompileGeometry(Span<const Vertex> vertices,
-                                   Span<const int> indices)
-{
-    struct GPU_Vertex {
-        float x, y;
-        float u, v;
-        uint8_t r, g, b, a;
-    };
-
-    auto vbo = GET_RENDER_API()->create_vertex_buffer();
-    vbo->alloc_cpu_buffer(sizeof(GPU_Vertex) * vertices.size());
-    auto buf = (GPU_Vertex*)vbo->get_cpu_buffer();
-    for (int i = 0; i < vertices.size(); i++) {
-        buf[i].x = vertices[i].position[0];
-        buf[i].y = vertices[i].position[1];
-        buf[i].r = vertices[i].colour[0];
-        buf[i].g = vertices[i].colour[1];
-        buf[i].b = vertices[i].colour[2];
-        buf[i].a = vertices[i].colour[3];
-        buf[i].u = vertices[i].tex_coord[0];
-        buf[i].v = vertices[i].tex_coord[1];
-    }
-
-    vbo->set_vertex_layout({
-        VertexAttr::POSITION_XY,
-        VertexAttr::UV,
-        VertexAttr::COLOR_BYTE_RGBA,
-    });
-
-    vbo->apply();
-    vbo->free_cpu_buffer();
-
-    IndiceBuffer* veo = GET_RENDER_API()->create_indice_buffer();
-    veo->alloc_cpu_buffer(indices.size());
-    auto indice_buf = veo->get_cpu_buffer();
-    for (int i = 0; i < indices.size(); i++) {
-        indice_buf[i] = indices[i];
-    }
-    veo->apply();
-    veo->free_cpu_buffer();
-
-    auto container = new VertexIndiceBuffer;
-    container->vbo = vbo;
-    container->veo = veo;
-
-    return (uintptr_t)container;
-}
-
-void MyRenderInterface::RenderGeometry(CompiledGeometryHandle geometry,
-                                       Vector2f translation,
-                                       TextureHandle texture)
-{
-    float offset[2] = { translation[0], translation[1] };
-
-    MaterialParam params[3] = {
-        { .name = "tex",
-          .value = { MaterialParamType::TEXTURE, { .tex_unit = 0 } } },
-        { .name = "offset",
-          .value = { MaterialParamType::VEC2, { .args = offset } } },
-        { .name = "canvas_size",
-          .value = { MaterialParamType::VEC2, { .args = canvas_size_ } } },
-    };
-
-    auto container = (VertexIndiceBuffer*)geometry;
-    GET_RENDER_API()->draw(DrawOperation {
-        .polygon_mode = PolygonMode::FILL,
-        .vertex_buffer = container->vbo,
-        .indice_buffer = container->veo,
-        .count = container->veo->get_indice_count(),
-        .texture = (Texture2D*)texture,
-        .textures = nullptr,
-        .material = material_,
-        .material_params = params,
-        .material_params_count = 3,
-    });
-
-    // LOG("draw %d %f %f",
-    //     container->veo->get_indice_count(),
-    //     translation[0],
-    //     translation[1]);
-}
-
-void MyRenderInterface::ReleaseGeometry(CompiledGeometryHandle geometry)
-{
-    // LOG("ReleaseGeometry");
-    auto container = (VertexIndiceBuffer*)geometry;
-    GET_RENDER_API()->destroy_vertex_buffer(container->vbo);
-    GET_RENDER_API()->destroy_indice_buffer(container->veo);
-    delete container;
-}
-
-TextureHandle MyRenderInterface::LoadTexture(Vector2i& texture_dimensions,
-                                             const String& source)
-{
-    // LOG("LoadTexture %s", source.c_str());
-    return 0;
-}
-
-TextureHandle MyRenderInterface::GenerateTexture(Span<const byte> source,
-                                                 Vector2i source_dimensions)
-{
-    // LOG("GenerateTexture %d %d %d",
-    //     source.size(),
-    //     source_dimensions[0],
-    //     source_dimensions[1]);
-    //
-    auto tex = GET_RENDER_API()->create_texture_2d();
-    tex->alloc_cpu_buffer(source_dimensions[0], source_dimensions[1]);
-    memcpy(tex->get_cpu_buffer(), source.data(), source.size());
-    tex->apply();
-    tex->free_cpu_buffer();
-    return (uintptr_t)tex;
-}
-
-void MyRenderInterface::ReleaseTexture(TextureHandle texture)
-{
-    GET_RENDER_API()->destroy_texture_2d((Texture2D*)texture);
-}
-
-void MyRenderInterface::EnableScissorRegion(bool enable) { }
-
-void MyRenderInterface::SetScissorRegion(Rectanglei region) { }
-
-}; // namespace anyone::rml_ui
--- anyone/main/rml_ui.h
+++ anyone/main/rml_ui.h
@@ -1,53 +1,12 @@
 #pragma once
-#include <RmlUi/Core.h>
 
 #include "platform/api/support.h"
 #include "main/core.h"
 
-namespace anyone::rml_ui {
-
-using namespace Rml;
-
-class MyRenderInterface : public Rml::RenderInterface {
-public:
-    MyRenderInterface();
-    ~MyRenderInterface();
-    CompiledGeometryHandle CompileGeometry(Span<const Vertex> vertices,
-                                           Span<const int> indices) override;
-
-    void RenderGeometry(CompiledGeometryHandle geometry,
-                        Vector2f translation,
-                        TextureHandle texture) override;
-
-    void ReleaseGeometry(CompiledGeometryHandle geometry) override;
-    TextureHandle LoadTexture(Vector2i& texture_dimensions,
-                              const String& source) override;
-    TextureHandle GenerateTexture(Span<const byte> source,
-                                  Vector2i source_dimensions) override;
-
-    void ReleaseTexture(TextureHandle texture) override;
-    void EnableScissorRegion(bool enable) override;
-    void SetScissorRegion(Rectanglei region) override;
-
-    void set_material(Material* m) { material_ = m; }
-    void set_canvas_size(const IntSize& s)
-    {
-        canvas_size_[0] = s.width;
-        canvas_size_[1] = s.height;
-    }
-
-private:
-    Material* material_;
-    float canvas_size_[2];
-};
-
-class MySystemInterface : public Rml::SystemInterface {
-public:
-    MySystemInterface();
-    ~MySystemInterface();
-};
-
-} // namespace anyone::rml_ui
+namespace Rml {
+class Context;
+class ElementDocument;
+}; // namespace Rml
 
 namespace anyone {
 
@@ -63,10 +22,6 @@ public:
     static void cleanup();
 
 private:
-    static rml_ui::MyRenderInterface* render_impl_;
-    static rml_ui::MySystemInterface* system_impl_;
-    static Material* rml_ui_material_;
-
     Rml::Context* context_;
     Rml::ElementDocument* document_;
     IntSize canvas_size_;
