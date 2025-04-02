a996eda8aa619a5a31a13fcc3910889d38615976
niu2x <niu2x@icloud.com>
2025-04-02 19:09:30 +0800

    fix

--- anyone/main/material.cpp
+++ anyone/main/material.cpp
@@ -1,10 +1,22 @@
 #include "material.h"
+#include "main/core.h"
 
 namespace anyone {
 
 Material::~Material() { }
 
-Material::Material() : blend_type_(BlendType::NONE) { }
+Material::Material() : blend_type_(BlendType::NONE), program_(nullptr) { }
+
+void Material::set_program(Program* p) { program_ = p; }
+
+void Material::set_blend_type(BlendType b) { blend_type_ = b; }
+
+void Material::use()
+{
+    auto api = GET_RENDER_API();
+    api->set_blend_type(blend_type_);
+    program_->use();
+}
 
 // void Material::use(RenderAPI* api) { api->set_blend_type(blend_type_); }
 
--- anyone/main/material.h
+++ anyone/main/material.h
@@ -28,16 +28,18 @@ namespace anyone {
 class Material {
 public:
     Material();
-    virtual ~Material();
-    virtual void set_param_texture(const char* name, int tex_unit) = 0;
-    virtual void set_param_vec2(const char* name, float args[]) = 0;
-    virtual void set_param_vec3(const char* name, float args[]) = 0;
-    virtual void set_param_vec4(const char* name, float args[]) = 0;
-    virtual void use();
-    void set_blend_type(BlendType b) { blend_type_ = b; }
+    ~Material();
+    void set_param_texture(const char* name, int tex_unit);
+    void set_param_vec2(const char* name, float args[]);
+    void set_param_vec3(const char* name, float args[]);
+    void set_param_vec4(const char* name, float args[]);
+    void use();
+    void set_blend_type(BlendType b);
+    void set_program(Program* p);
 
 private:
     BlendType blend_type_;
+    Program* program_;
 };
 
 } // namespace anyone
