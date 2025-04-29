d993da191ef9ab41edea24a966f87804feb2c4ea
niu2x <niu2x@icloud.com>
2025-04-29 12:28:42 +0800

    fix

--- anyone/main/material.h
+++ anyone/main/material.h
@@ -4,27 +4,6 @@
 
 namespace anyone {
 
-// class Material : public Ref {
-// public:
-//     Material();
-//     ~Material();
-
-//     void set_program(GL_Program* p);
-//     void use();
-
-//     void set_uniform(const UniformValue& uniform);
-//     void unset_uniform(const String& name);
-//     void compile();
-//     const char* get_type() const override { return "Material"; }
-//     void set_blend_type(BlendType b) { blend_ = b; }
-
-// private:
-//     GL_Program* program_;
-//     std::unordered_map<String, UniformValue> uniforms_;
-
-//     BlendType blend_;
-// };
-
 class Material {
 public:
     Material();
